e9e2cc09675aa7b50da2aed635d08895713ead83
Camden Cheek <ccheek22@gmail.com>
2016-05-16 22:04:05 -0500

    Finished problem 012

--- p012/p012.cpp
+++ p012/p012.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <set>
+#include <unordered_map>
 
 using namespace std;
 
@@ -8,46 +8,86 @@ int nth_triangular(int n) {
   return n*(n+1)/2;
 }
 
-long smallestFactor(long n) {
-  long i = 2;
-  while (true) {
-    if (n % i == 0) {
-      return i;
-    }
-    i++;
-  }
-}
+struct Primes {
+protected:
+  vector<int> prime_list;
 
-set<long> factors(long n) {
-  set<long> factors;
-  while (true) {
-    long sm = smallestFactor(n);
-    factors.insert(sm);
-    if (sm == n) {
-      return factors;
-    } else {
-      n /= sm;
+  void append_next() {
+    int biggest = *(prime_list.end() - 1) + 2;
+    while (!is_prime(biggest)) {
+      biggest += 2;
+    }
+    prime_list.push_back(biggest);
+  }
+
+public:
+
+  Primes() {
+    prime_list.push_back(2);
+    prime_list.push_back(3);
+  }
+
+  bool is_prime(int n) {
+    int i = 0;
+    while (prime_list[i]*prime_list[i] <= n) {
+      if (n % prime_list[i] == 0)
+	return false;
+      i++;
+    }
+    return true;
+  }
+
+  int get_nth(int n) {
+    while (prime_list.size() < n) {
+      append_next();
+    }
+    return prime_list[n - 1];
+  }
+
+  int smallest_prime_factor(int n) {
+    int i = 1;
+    while (true) {
+      int curr = get_nth(i);
+      if (n % get_nth(i) == 0)
+        return curr;
+      i++;
     }
   }
+
+  unordered_map<int,int> prime_factors(int n) {
+    unordered_map<int,int> factor_list;
+    while (n != 1) {
+      int factor = smallest_prime_factor(n);
+      factor_list.emplace(factor,0);
+      factor_list[factor]++;
+
+      n /= factor;
+    }
+    return factor_list;
+  }
+};
+
+int num_factors_from_prime_factors(unordered_map<int,int> prime_factors) {
+  int total = 1;
+  for (auto i: prime_factors) {
+    total *= (i.second + 1);
+  }
+  return total + 1;
 }
 
 int main() {
-  int num_factors = 0;
-//  int i = 2;
-//  long curr;
-//  while ( num_factors <= 500 ) {
-//    curr = nth_triangular(i);
-//    set<long> factor_set = factors(curr);
-//    num_factors = factor_set.size();
-//    cout << curr << ": " << num_factors << endl;
-//    i++;
-//  }
-//  cout << "Solution: " << curr << endl;
-
-  auto factor_set = factors(28);
-  for(auto iter=factor_set.begin(); iter!=factor_set.end();++iter) {
-    cout << *iter << endl;
+  Primes myprimes;
+  int num_factors = 1;
+  int i = 2;
+  int num;
+  while (num_factors <= 500) {
+    num = nth_triangular(i);
+    auto factors = myprimes.prime_factors(num);
+    num_factors = num_factors_from_prime_factors(factors);
+    i++;
   }
+
+  cout << num << endl;
 
   return 0;
 }
