e49889b9a56f8782465a9587af3a87169b721a02
Camden Cheek <ccheek22@gmail.com>
2016-05-21 13:00:24 -0400

    Finished p022

--- p022/p022.cpp
+++ p022/p022.cpp
@@ -2,8 +2,26 @@
 #include <fstream>
 #include <vector>
 #include <boost/algorithm/string.hpp>
+#include <unordered_map>
 
 using namespace std;
+
+
+int name_score(string name) {
+  static unordered_map<char,int> alphabet_score;
+  if (!alphabet_score.size()) {
+    for (int i = 0; i < 26; i++) {
+      alphabet_score.insert({'A' + i, i + 1});
+    }
+  }
+
+  int score = 0;
+  for(char i: name) {
+      score += alphabet_score[i];
+  }
+
+  return score;
+}
 
 int main() {
   ifstream names_file("/Users/ccheek/project_euler/p022/p022_names.txt");
@@ -17,6 +35,15 @@ int main() {
   }
   names_file.close();
 
+  sort(names_vector.begin(),names_vector.end());
+
+  int total = 0;
+  for (int i = 0; i < names_vector.size(); i++) {
+    total += (i+1) * name_score(names_vector[i]);
+
+  }
+
+  cout << total << endl;
 
 
 }
