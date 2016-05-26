e7f1867a04c565b735a450750e9e7d1211e1b71d
Camden Cheek <ccheek22@gmail.com>
2016-05-26 15:18:31 -0400

    Finished p025

---
+++ p025/p025.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <gmp.h>
+#include <boost/timer.hpp>
+
+using namespace std;
+
+
+int main() {
+  boost::timer t;
+  mpz_t a;
+  mpz_init(a);
+  int  i = 1;
+  do {
+    mpz_fib_ui(a,i);
+    i++;
+  } while (mpz_sizeinbase(a,10) < 1000);
+  cout << i << endl << t.elapsed() << endl;
+}
