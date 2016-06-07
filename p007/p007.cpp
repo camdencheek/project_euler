4200c21efbd7e383565a3872cc46c0ceb546007b
Camden Cheek <ccheek22@gmail.com>
2016-06-07 00:05:47 -0400

    Used primes.h

--- p007/p007.cpp
+++ p007/p007.cpp
@@ -1,47 +1,17 @@
 #include <iostream>
 #include <vector>
+#include "../headers/primes.h"
+#include <boost/timer.hpp>
 
 using namespace std;
 
-struct Primes {
-  vector<int> prime_list;
-
-  Primes() {
-    prime_list.push_back(2);
-    prime_list.push_back(3);
-  }
-  
-  bool is_prime(int n) {
-    int i = 0;
-    while (prime_list[i]*prime_list[i] <= n) {
-      if (n % prime_list[i] == 0)
-	return false;
-      i++;
-    }
-    return true;
-  }
-
-  int get_nth(int n) {
-    while (prime_list.size() < n) {
-      append_next();
-    }
-    return prime_list[n - 1];
-  }
-
-  void append_next() {
-    int biggest = *(prime_list.end() - 1) + 2;
-    while (!is_prime(biggest)) {
-      biggest += 2;
-    }
-    prime_list.push_back(biggest);
-  }
-};
-
 int main() {
 
-  Primes my_primes;
+	boost::timer t;
+	Primes my_primes;
 
-  cout << my_primes.get_nth(10001) << endl;
+	cout << my_primes.get_nth(10001) << endl;
+	cout << t.elapsed()*1000 << "ms" << endl;
 
   
 }
