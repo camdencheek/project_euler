793be8568b03a351fd0236f5a8862de7503996eb
Camden Cheek <ccheek@camdens-air.bluemedora.localnet>
2016-08-18 10:05:39 -0400

    Added a couple files

--- headers/fraction.h
+++ headers/fraction.h
@@ -1,18 +1,43 @@
 #ifndef CAM_FRACTION
 #define CAM_FRACTION
 
+#include <boost/math/common_factor_rt.hpp>
+#include <string>
 
-class fraction {
+class Fraction {
 	
-	int numerator;
-	int denominator;
+public:
+	long numerator;
+	long denominator;
 
+	Fraction(long numerator, long denominator) {
+		this->numerator = numerator;
+		this->denominator = denominator;
+
+		this->reduce();
+	}
+
+	void reduce() { 
+		int divisor = boost::math::gcd(numerator,denominator);
+		numerator /= divisor;
+		denominator /= divisor;
+	}
+
+	std::string to_string() {
+		return std::to_string(this->numerator) + "/" + std::to_string(this->denominator);
+	}
+
+	Fraction operator*(const Fraction& f) {
+		Fraction new_fraction(this->numerator * f.numerator,this->denominator * f.denominator);
+		return new_fraction;
+	}
+
+	Fraction operator/(const Fraction& f) {
+		Fraction new_fraction(this->numerator * f.denominator,this->denominator * f.numerator);
+		return new_fraction;
+	}
 	
-
-
-
-
-}
+};
 
 
 
---
+++ tests/fraction_test.cpp
@@ -0,0 +1,8 @@
+#include "../headers/fraction.h"
+#include "test_main.hpp"
+
+TEST_CASE("Fractions can be created","[fraction]") {
+	SECTION("Fraction is created") {
+		Fraction f(1,2);
+	}
+}
